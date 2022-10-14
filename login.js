1e5cedcba5c3e671d5b4912e48f76d202d737d83
tungpho6 <113914198+tungpho6@users.noreply.github.com>
2022-10-14 23:49:17 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,22 @@
+let objPeople = [
+{ username: "211210244",
+  password: 'ductung05'
+},
+{ username: "Phan Dong Hai",
+  password: 'phandonghai'
+},
+
+
+];
+function Login (){
+let username = document.getElementById('User-name').value;
+let password = document.getElementById('Password').value;
+objPeople.forEach( function(objPeople){
+if(username == objPeople.username && password == objPeople.password){
+window.location.href = "https://tungpho6.github.io/Student-management/Studentmanage.html";
+}else{
+alert('User or password may be not valid! ');  
+}
+
+})
+};
